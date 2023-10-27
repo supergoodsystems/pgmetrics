8a3b986a3323f92309ed64356a3bdfebd6d865d2
Alex Klarfeld <alex.klarfeld@gmail.com>
2023-10-26 21:40:14 -0700

    Fixed errant text

--- collector/collect.go
+++ collector/collect.go
@@ -242,7 +242,6 @@ func Collect(o CollectConfig, dbnames []string) *pgmetrics.Model {
 
 func getConn(connstr string, o CollectConfig) *sql.DB {
 	// connect
-	fmt.Println("Connection String: " + connstr)
 	db, err := sql.Open("postgres", connstr)
 	if err != nil {
 		log.Fatal(err)
