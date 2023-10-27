a14f823c7592f25866c3f6a0d55b7373d59366ea
Alex Klarfeld <alex.klarfeld@gmail.com>
2023-10-26 19:20:46 -0700

    Added build and start scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-curl -O -L https://github.com/rapidloop/pgdash/releases/download/v1.11.0/pgdash_1.11.0_linux_amd64.tar.gz tar xvf pgdash_1.11.0_linux_amd64.tar.gz
+curl -O -L https://github.com/rapidloop/pgdash/releases/download/v1.11.0/pgdash_1.11.0_linux_amd64.tar.gz >> /var/data
+tar xvf /var/data/pgdash_1.11.0_linux_amd64.tar.gz
 go build cmd/pgmetrics/main.go
---
+++ start.sh
@@ -0,0 +1,1 @@
+./main -f json | /var/data/pgdash_1.11.0_linux_amd64/pgdash -a $PGDASH_API_KEY report neondb
